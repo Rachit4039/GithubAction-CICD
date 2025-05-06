047c39b9aee7ef73a545e72d0915b71605f165aa
rachit <rachit2004.joshi@gmail.com>
2025-05-06 16:46:31 +0530

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div>
+      <h1>Rachit Joshi</h1>
+      <h1>My React App</h1>
+    </div>
+  )
+}
+
+export default App
